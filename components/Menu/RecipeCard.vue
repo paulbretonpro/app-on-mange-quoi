50c448f3556d1fb3653bb197e00c9defb61a8f07
paulbretonpro <paulobreton29800@gmail.com>
2024-08-15 00:23:45 +0200

    chore: test loader

--- components/Menu/RecipeCard.vue
+++ components/Menu/RecipeCard.vue
@@ -6,7 +6,7 @@ defineProps<{
 
 <template>
   <div
-    class="border dark:border-gray-800 rounded-md w-full px-4 py-2 flex gap-4 justify-between"
+    class="border dark:border-gray-800 rounded-md w-full px-4 py-2 flex gap-4 justify-between items-center"
   >
     <div>{{ name }}</div>
     <UButton icon="i-heroicons-trash" variant="ghost" color="red" />
--- pages/menus/[id].vue
+++ pages/menus/[id].vue
@@ -10,114 +10,120 @@ interface IMenuByDay {
 
 const route = useRoute();
 
-const defaultPlanningByDay: IMenuByDay[] = [
-  {
-    day: 0,
-    dayName: "Lundi",
-    lunch: [],
-    dinner: [],
-  },
-  {
-    day: 1,
-    dayName: "Mardi",
-    lunch: [],
-    dinner: [],
-  },
-  {
-    day: 2,
-    dayName: "Mercredi",
-    lunch: [],
-    dinner: [],
-  },
-  {
-    day: 3,
-    dayName: "Jeudi",
-    lunch: [],
-    dinner: [],
-  },
-  {
-    day: 4,
-    dayName: "Vendredi",
-    lunch: [],
-    dinner: [],
-  },
-  {
-    day: 5,
-    dayName: "Samedi",
-    lunch: [],
-    dinner: [],
-  },
-  {
-    day: 6,
-    dayName: "Dimanche",
-    lunch: [],
-    dinner: [],
-  },
-];
-
-const { data } = useFetch<IMenuRecipes | null>(
+const { data, status } = await useLazyFetch<IMenuRecipes | null>(
   `/api/menus/${route.params.id}/recipes`
 );
 
 const planningByDay = computed<IMenuByDay[]>(() => {
-  return data.value?.recipes.reduce(
-    (acc, menu: { day: number; lunch: boolean; recipe: IRecipe }) => {
+  const defaultPlanningByDay: IMenuByDay[] = [
+    {
+      day: 0,
+      dayName: "Lundi",
+      lunch: [],
+      dinner: [],
+    },
+    {
+      day: 1,
+      dayName: "Mardi",
+      lunch: [],
+      dinner: [],
+    },
+    {
+      day: 2,
+      dayName: "Mercredi",
+      lunch: [],
+      dinner: [],
+    },
+    {
+      day: 3,
+      dayName: "Jeudi",
+      lunch: [],
+      dinner: [],
+    },
+    {
+      day: 4,
+      dayName: "Vendredi",
+      lunch: [],
+      dinner: [],
+    },
+    {
+      day: 5,
+      dayName: "Samedi",
+      lunch: [],
+      dinner: [],
+    },
+    {
+      day: 6,
+      dayName: "Dimanche",
+      lunch: [],
+      dinner: [],
+    },
+  ];
+  data.value?.recipes.forEach(
+    (menu: { day: number; lunch: boolean; recipe: IRecipe }) => {
       const day = menu.day;
       if (day === 0) {
-        if (menu.lunch) acc[0].lunch.push(menu.recipe);
-        else acc[0].dinner.push(menu.recipe);
+        if (menu.lunch) defaultPlanningByDay[0].lunch.push(menu.recipe);
+        else defaultPlanningByDay[0].dinner.push(menu.recipe);
       } else if (day === 1) {
-        if (menu.lunch) acc[1].lunch.push(menu.recipe);
-        else acc[1].dinner.push(menu.recipe);
+        if (menu.lunch) defaultPlanningByDay[1].lunch.push(menu.recipe);
+        else defaultPlanningByDay[1].dinner.push(menu.recipe);
       } else if (day === 2) {
-        if (menu.lunch) acc[2].lunch.push(menu.recipe);
-        else acc[2].dinner.push(menu.recipe);
+        if (menu.lunch) defaultPlanningByDay[2].lunch.push(menu.recipe);
+        else defaultPlanningByDay[2].dinner.push(menu.recipe);
       } else if (day === 3) {
-        if (menu.lunch) acc[3].lunch.push(menu.recipe);
-        else acc[3].dinner.push(menu.recipe);
+        if (menu.lunch) defaultPlanningByDay[3].lunch.push(menu.recipe);
+        else defaultPlanningByDay[3].dinner.push(menu.recipe);
       } else if (day === 4) {
-        if (menu.lunch) acc[4].lunch.push(menu.recipe);
-        else acc[4].dinner.push(menu.recipe);
+        if (menu.lunch) defaultPlanningByDay[4].lunch.push(menu.recipe);
+        else defaultPlanningByDay[4].dinner.push(menu.recipe);
       } else if (day === 5) {
-        if (menu.lunch) acc[5].lunch.push(menu.recipe);
-        else acc[5].dinner.push(menu.recipe);
+        if (menu.lunch) defaultPlanningByDay[5].lunch.push(menu.recipe);
+        else defaultPlanningByDay[5].dinner.push(menu.recipe);
       } else if (day === 6) {
-        if (menu.lunch) acc[6].lunch.push(menu.recipe);
-        else acc[6].dinner.push(menu.recipe);
+        if (menu.lunch) defaultPlanningByDay[6].lunch.push(menu.recipe);
+        else defaultPlanningByDay[6].dinner.push(menu.recipe);
       }
-      return acc;
-    },
-    defaultPlanningByDay
-  ) as IMenuByDay[];
+    }
+  );
+
+  return defaultPlanningByDay;
 });
 </script>
 
 <template>
-  <div class="text-3xl font-extrabold mb-6">Semaine {{ data?.weekNumber }}</div>
-  <div
-    v-for="menu in planningByDay"
-    :key="menu.dayName"
-    class="flex flex-col gap-2 mb-12 w-full"
-  >
-    <div class="text-sm md:text-base font-medium flex items-center gap-2">
-      <UIcon name="i-heroicons-calendar" size="sm" />
-      <div>{{ menu.dayName }}</div>
+  <div v-if="status === 'pending'">
+    <Loader />
+  </div>
+  <template v-else>
+    <div class="text-3xl font-extrabold mb-6">
+      Semaine {{ data?.weekNumber }}
     </div>
     <div
-      v-if="menu.lunch.length === 0 && menu.dinner.length === 0"
-      class="text-sm text-gray-400 dark:text-gray-600"
+      v-for="menu in planningByDay"
+      :key="menu.dayName"
+      class="flex flex-col gap-2 mb-12 w-full"
     >
-      Aucun repas
-    </div>
-    <div v-else class="grid grid-cols-2 gap-4">
-      <div class="flex flex-col gap-2 grow">
-        <span class="text-xs text-gray-400 dark:text-gray-600">Midi</span>
-        <MenuRecipeCard v-for="lunch in menu.lunch" :name="lunch.name" />
+      <div class="text-sm md:text-base font-medium flex items-center gap-2">
+        <UIcon name="i-heroicons-calendar" size="sm" />
+        <div>{{ menu.dayName }}</div>
       </div>
-      <div class="flex flex-col gap-2 grow">
-        <span class="text-xs text-gray-400 dark:text-gray-600">Soir</span>
-        <MenuRecipeCard v-for="dinner in menu.dinner" :name="dinner.name" />
+      <div
+        v-if="menu.lunch.length === 0 && menu.dinner.length === 0"
+        class="text-sm text-gray-400 dark:text-gray-600"
+      >
+        Aucun repas
+      </div>
+      <div v-else class="grid grid-cols-2 gap-4">
+        <div class="flex flex-col gap-2 grow">
+          <span class="text-xs text-gray-400 dark:text-gray-600">Midi</span>
+          <MenuRecipeCard v-for="lunch in menu.lunch" :name="lunch.name" />
+        </div>
+        <div class="flex flex-col gap-2 grow">
+          <span class="text-xs text-gray-400 dark:text-gray-600">Soir</span>
+          <MenuRecipeCard v-for="dinner in menu.dinner" :name="dinner.name" />
+        </div>
       </div>
     </div>
-  </div>
+  </template>
 </template>
